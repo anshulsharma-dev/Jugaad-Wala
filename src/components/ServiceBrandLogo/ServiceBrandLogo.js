25866b8f12442950df49d67a5366339937a46369
Your Name <sharmaanshul9796@gmail.com>
2024-07-18 17:08:06 +0530

    gh

--- src/components/ServiceBrandLogo/ServiceBrandLogo.js
+++ src/components/ServiceBrandLogo/ServiceBrandLogo.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React from "react";
 
-const ServiceBrandLogo = ({logo_num}) => {
-    return (
-        <>
-            <div className="brand-wrapper-two text-center swiper-slide wow fadeInUp" data-wow-delay="1.8s" data-swiper-autoplay="5000">
-                <a href="#"><img src={`assets/img/brand/brand-${logo_num}.png`} className="img-fluid" alt="img" /></a>
-            </div>
-        </>
-    );
+const ServiceBrandLogo = ({ logo_num }) => {
+  return (
+    <>
+      <div
+        className="brand-wrapper-two text-center swiper-slide wow fadeInUp"
+        data-wow-delay="1.8s"
+        data-swiper-autoplay="5000"
+      >
+        <a href="javascript:void(0);">
+          <img
+            src={`assets/img/brand/brand-${logo_num}.png`}
+            className="img-fluid"
+            alt="img"
+          />
+        </a>
+      </div>
+    </>
+  );
 };
 
 export default ServiceBrandLogo;
